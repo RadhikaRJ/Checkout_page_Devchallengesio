2ad7a2615b4c35161723fcc9554385dcecc197e0
RadhikaRJ <radhika7002@gmail.com>
2021-02-10 23:06:54 +0530

    incrementBagQuantity

--- app.js
+++ app.js
@@ -6,13 +6,31 @@ var bagQuantityRef = document.querySelector("#bagQuantity");
 
 btnIncBagRef.addEventListener("click",incrementBagQuantity);
 
-btnDecBagRef.addEventListener("click",decrementBagQuantity);
+btnDecBagRef.addEventListener("click",decrementBagQuantity());
 
 var btnDecShoesRef = document.querySelector("#btnDecShoes");
 var btnIncShoesRef = document.querySelector("#btnIncShoes");
 var shoesQuantityRef = document.querySelector("#shoesQuantity");
 
-btnIncShoesRef.addEventListener("click",incrementShoesQuantity);
+btnIncShoesRef.addEventListener("click",incrementShoesQuantity());
 
-btnDecShoesRef.addEventListener("click",decrementShoesQuantity);
+btnDecShoesRef.addEventListener("click",decrementShoesQuantity());
 
+
+function incrementBagQuantity(){
+    var currentBagQuantity=document.getElementById('bagQuantity').value;
+    console.log(currentBagQuantity);
+    var finalBagQuantity=Number(currentBagQuantity)+1;
+    bagQuantityRef.innerHTML=Number(finalBagQuantity);
+    var currentShoesQuantity=shoesQuantityRef.value;
+    updateTotalPrice(currentShoesQuantity,finalBagQuantity);
+
+}
+
+
+
+
+function updateTotalPrice(quantityOfShoes,quantityOfBags){
+    var totalAmount = 19+54.99*Number(quantityOfBags)+74.99*Number(quantityOfShoes);
+   totalAmtRef.innerHTML=totalAmount;
+}

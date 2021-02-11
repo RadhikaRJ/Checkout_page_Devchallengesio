41cf1a8a175bbfc878f0733015ae5b3a62cb3627
RadhikaRJ <radhika7002@gmail.com>
2021-02-11 07:12:21 +0530

    feature :shoesIncrement

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bagQuantityRef = document.querySelector("#bagQuantity");
 
 btnIncBagRef.addEventListener("click",incrementBagQuantity);
 
-btnDecBagRef.addEventListener("click",decrementBagQuantity);
+// btnDecBagRef.addEventListener("click",decrementBagQuantity);
 
 var btnDecShoesRef = document.querySelector("#btnDecShoes");
 var btnIncShoesRef = document.querySelector("#btnIncShoes");
@@ -14,12 +14,12 @@ var shoesQuantityRef = document.querySelector("#shoesQuantity");
 
 btnIncShoesRef.addEventListener("click",incrementShoesQuantity);
 
-btnDecShoesRef.addEventListener("click",decrementShoesQuantity);
+// btnDecShoesRef.addEventListener("click",decrementShoesQuantity);
 
 
 function incrementBagQuantity(){
     var currentBagQuantity=document.getElementById('bagQuantity').innerText;
-    console.log(currentBagQuantity);
+    //console.log(currentBagQuantity);
     var finalBagQuantity=Number(currentBagQuantity)+1;
     bagQuantityRef.innerHTML=Number(finalBagQuantity);
     var currentShoesQuantity=document.getElementById('shoesQuantity').innerText;
@@ -28,7 +28,14 @@ function incrementBagQuantity(){
 }
 
 
-
+function incrementShoesQuantity(){
+    var currentShoesQuantity=document.getElementById('shoesQuantity').innerText;
+    // console.log(currentShoesQuantity);
+    var finalShoesQuantity=Number(currentShoesQuantity)+1;
+    shoesQuantityRef.innerHTML=Number(finalShoesQuantity);
+    var currentBagQuantity=document.getElementById('bagQuantity').innerText;
+    updateTotalPrice(finalShoesQuantity,currentBagQuantity);
+}
 
 function updateTotalPrice(quantityOfShoes,quantityOfBags){
     var totalAmount = 19+54.99*Number(quantityOfBags)+74.99*Number(quantityOfShoes);
